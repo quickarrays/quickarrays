df0df12f9280c5dbca92060edde24493bc5e3de6
Jonas Ellert <jonas.ellert@tu-dortmund.de>
2026-02-24 15:35:14 +0100

    fix NSS and PSS display

--- assets/ds_list.js
+++ assets/ds_list.js
@@ -7,13 +7,14 @@ class DataStructureList extends ItemList {
             if (el.classList.contains('qa-structure-length')) return 'length';
             if (el.classList.contains('qa-structure-factor')) return 'factor';
             if (el.classList.contains('qa-structure-position')) return 'position';
+            if (el.classList.contains('qa-structure-other-index')) return 'other';
             return null;
         });
     }
 }
 
-DataStructureList.prototype.isIndex = function(dsName) { return this.hasClass(dsName, 'qa-structure-index'); };
-DataStructureList.prototype.isLength = function(dsName) { return this.hasClass(dsName, 'qa-structure-length'); };
-DataStructureList.prototype.isString = function(dsName) { return this.hasClass(dsName, 'qa-structure-string'); };
-DataStructureList.prototype.isFactorization = function(dsName) { return this.hasClass(dsName, 'qa-structure-factor'); };
-DataStructureList.prototype.isPosition = function(dsName) { return this.hasClass(dsName, 'qa-structure-position'); };
+DataStructureList.prototype.isIndex = function (dsName) { return this.hasClass(dsName, 'qa-structure-index') || this.hasClass(dsName, 'qa-structure-other-index'); };
+DataStructureList.prototype.isLength = function (dsName) { return this.hasClass(dsName, 'qa-structure-length'); };
+DataStructureList.prototype.isString = function (dsName) { return this.hasClass(dsName, 'qa-structure-string'); };
+DataStructureList.prototype.isFactorization = function (dsName) { return this.hasClass(dsName, 'qa-structure-factor'); };
+DataStructureList.prototype.isPosition = function (dsName) { return this.hasClass(dsName, 'qa-structure-position'); };
